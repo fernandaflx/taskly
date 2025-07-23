7c8fa76de55290835d2959a4aa7af34714928ea7
Fernanda Felix <fernandaleitefelix@gmail.com>
2025-07-23 14:56:42 -0300

    feat: cerate middleware to protect logged pages

---
+++ src/app/middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+const protectedRoutes = ['/dashboard', '/tasks', '/notes']
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get('token')?.value
+
+  const isProtected = protectedRoutes.some((route) =>
+    request.nextUrl.pathname.startsWith(route)
+  )
+
+  if (isProtected && !token) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/dashboard/:path*', '/tasks/:path*', '/notes/:path*'],
+}
--- src/features/auth/actions.ts
+++ src/features/auth/actions.ts
@@ -1,13 +1,20 @@
 import { auth } from '@/lib/firebase'
 import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
-
-const googleProvider = new GoogleAuthProvider()
+import { setCookie } from 'cookies-next'
 
 export async function signInWithGoogle() {
   try {
+    const googleProvider = new GoogleAuthProvider()
     const result = await signInWithPopup(auth, googleProvider)
-    // const user = result.user
-    return result.user
+    const user = result.user
+    const token = await user.getIdToken()
+
+    setCookie('token', token, {
+      maxAge: 60 * 60,
+      path: '/',
+    })
+
+    return user
   } catch (error) {
     console.error('Erro no login com Google:', error)
     throw error
