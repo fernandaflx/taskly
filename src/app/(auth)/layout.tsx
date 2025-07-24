e32e6e12c8a964749b8da0dd4279012b7aea1dca
Fernanda Felix <fernandaleitefelix@gmail.com>
2025-07-24 02:47:59 -0300

    feat: create and setup dashboard page

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,30 @@
 'use client'
 
+import clsx from 'clsx'
 import { AppSidebar } from "@/components/Sidebar"
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { SidebarProvider, SidebarTrigger, useSidebar } from "@/components/ui/sidebar"
 import RootClient from "@/store/rootClient"
 import { useUserStore } from "@/store/useUserStore"
+
+function LayoutContent({ children }: { children: React.ReactNode }) {
+  const { open } = useSidebar()
+  console.log(open)
+
+  return (
+    <main
+      className={clsx(
+        "min-h-screen w-full transition-all duration-200 ease-in-out",
+        open
+          ? "max-w-[calc(100vw-12rem)]"
+          : "max-w-[calc(100vw-3rem)]"
+      )}
+    >
+      <SidebarTrigger />
+      <RootClient />
+      {children}
+    </main>
+  )
+}
 
 export default function AuthenticatedLayout({ children }: { children: React.ReactNode }) {
   const { user } = useUserStore()
@@ -13,11 +34,7 @@ export default function AuthenticatedLayout({ children }: { children: React.Reac
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main>
-        <SidebarTrigger />
-        <RootClient />
-        {children}
-      </main>
+      <LayoutContent>{children}</LayoutContent>
     </SidebarProvider>
   )
 }
